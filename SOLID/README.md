327dccb962ffcaf5a2f70ad886493fd99b879c5d
taiseen <taiseen.cse@gmail.com>
2023-05-29 00:35:39 +0600

    ✨ feat: SOLID Principel - Single Responsibility

---
+++ SOLID/README.md
@@ -0,0 +1,16 @@
+> 29 - May - 2023
+
+# SOLID Principles
+* Single Responsibility Principle (SRP)
+* Open-Closed Principle (OCP)
+* Liskov Substitution Principle (LSP)
+* Interface Segregation Principle (ISP)
+* Dependency Inversion Principle (DIP)
+
+```
+Applying SOLID principles can lead to code that is 
+more modular, 
+maintainable and 
+extensible, 
+and it can make it easier for developers to work collaboratively on a codebase.
+```
---
+++ SOLID/SingleResponsibility/Marker.ts
@@ -0,0 +1,112 @@
+// 29 - May - 2023
+
+// Single Responsibility Principle - SRP
+
+class Marker {
+    constructor(
+        public name: string,
+        public color: string,
+        public price: number
+    ) { }
+}
+
+// #######################################################
+// #######################################################
+// #######################################################
+
+// class Invoice {
+
+//     constructor(private marker: Marker, private quantity: number) { }
+
+//     calculateTotal(): number {
+//         return this.marker.price * this.quantity;
+//     }
+
+//     printInvoice(): void {
+//         // printing implementation
+//     }
+
+//     saveToDb(): void {
+//         // save to database implementation
+//     }
+// }
+
+
+// #######################################################
+// #######################################################
+// #######################################################
+
+
+/*
+In this refactored, 
+we have split the responsibilities of the Invoice class into 3 separate classes: 
+Invoice, InvoiceDao, and InvoicePrinter.
+
+The Invoice class is responsible only for calculating the total amount, 
+and the printing and saving responsibilities have been delegated to separate classes. 
+This makes the code more modular, easier to understand, and less prone to errors.
+*/ 
+
+class Invoice {
+
+    constructor(public marker: Marker, public quantity: number) { }
+
+    calculateTotal(): number {
+        const total = this.marker.price * this.quantity;
+        console.log(total);
+        return total;
+    }
+}
+
+// #######################################################
+// #######################################################
+// #######################################################
+
+class InvoiceSave {
+
+    constructor(invoice: Invoice) { }
+
+    saveToDb(): void {
+        console.log('save to database...');
+    }
+}
+
+// #######################################################
+// #######################################################
+// #######################################################
+
+class InvoicePrinter {
+
+    constructor(public invoice: Invoice) { }
+
+    printInvoice(): void {
+        // console.log(invoice);
+        console.log(`
+            Marker Info:-
+            Name: ${this.invoice.marker.name}   
+            Color: ${this.invoice.marker.color}   
+            Price: ${this.invoice.marker.price}   
+            Quantity: ${this.invoice.quantity}
+
+            Total Price: ${this.invoice.calculateTotal()}
+        `);
+    }
+}
+
+// #######################################################
+// #######################################################
+// #######################################################
+
+const pen = new Marker('BollPen', 'Black', 30);
+
+
+const invoice = new Invoice(pen, 2);
+invoice.calculateTotal();
+
+
+const invoicePrint = new InvoicePrinter(invoice);
+invoicePrint.printInvoice();
+
+
+const invoiceSave = new InvoiceSave(invoice);
+invoiceSave.saveToDb();
