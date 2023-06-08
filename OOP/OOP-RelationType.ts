3d00aa227fbb432ed97fe606a40ac1258d8a71e8
taiseen <taiseen.cse@gmail.com>
2023-06-08 21:12:16 +0600

    feat: oop relation type

---
+++ OOP/OOP-RelationType.ts
@@ -0,0 +1,27 @@
+// 30 - May - 2023
+
+/*
+OOP ==> Relationship Types
+
+One of the advantages of Object-Oriented programming language is code reuse. 
+This reusability is possible due to the relationship b/w the classes. 
+
+OR Reusability is feasible because various types of relationships 
+that can be implemented among classes.
+
+OOP generally support 4 types of relationships that are:
+inheritance,  
+composition,
+aggregation &
+association.
+Dependency
+
+
+All these relationship is based on 
+"is a" relationship, 
+"has-a" relationship & 
+"part-of" relationship.
+*/
+
+
+
